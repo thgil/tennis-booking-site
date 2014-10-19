d569ea7e9c40b8ae75e297a5ecc3837c85b12c3f
Fergus Leen <leenf@tcd.ie>
2014-10-19 15:28:49 +0100

    Init

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('tennisBookingSiteApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
